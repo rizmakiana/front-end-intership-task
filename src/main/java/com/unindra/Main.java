0f627a1c7a246c3b5dd8633b01c98a6a6ece8340
Rizky Maulana <rizky1517@gmail.com>
2025-10-12 16:24:30 +0700

    refactor(main): refactor Main class name.

---
+++ src/main/java/com/unindra/Main.java
@@ -0,0 +1,21 @@
+package com.unindra;
+
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import com.unindra.view.LoginView;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main {
+
+	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+            
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+	
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            LoginView view = new LoginView();
+            view.setVisible(true);
+            
+	}
+
+}
