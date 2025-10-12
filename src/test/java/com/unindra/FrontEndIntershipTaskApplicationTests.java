be47eb9a77b1d053cd923fae1466fcd5276a8822
Rizky Maulana <rizky1517@gmail.com>
2025-10-12 12:27:57 +0700

    set up project

---
+++ src/test/java/com/unindra/FrontEndIntershipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndIntershipTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
