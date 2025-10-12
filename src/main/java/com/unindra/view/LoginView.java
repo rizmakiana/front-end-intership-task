0969d9e88c3e9f9c39b79a32d6ec126f830adfa1
Rizky Maulana <rizky1517@gmail.com>
2025-10-12 16:25:37 +0700

    feature(login): add login view

---
+++ src/main/java/com/unindra/view/LoginView.java
@@ -0,0 +1,112 @@
+package com.unindra.view;
+
+import com.unindra.util.AppManager;
+import java.awt.Image;
+
+/**
+ *
+ * @author rizmakiana
+ */
+public class LoginView extends javax.swing.JFrame {
+
+    public LoginView() {
+        generateComponent();
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        image = new javax.swing.JLabel();
+        corner = new javax.swing.JLabel();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Management School App");
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setUndecorated(true);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        image.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/images/image-login.png"))); // NOI18N
+        getContentPane().add(image, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 20, -1, -1));
+        getContentPane().add(corner, new org.netbeans.lib.awtextra.AbsoluteConstraints(1280, 720, -1, -1));
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Adwaita Sans", 1, 32)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Masuk");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(256, 90, 150, 50));
+        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 280, 300, 40));
+        jPanel1.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 355, 300, 40));
+
+        jCheckBox1.setText("Tampilkan kata sandi");
+        jPanel1.add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 400, -1, -1));
+
+        jLabel2.setText("Nama pengguna");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 255, -1, -1));
+
+        jLabel3.setText("Kata sandi");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 330, -1, -1));
+
+        jButton1.setText("Masuk");
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(181, 450, 300, 40));
+
+        jTabbedPane1.addTab("tab1", jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 640, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 685, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("tab2", jPanel2);
+
+        getContentPane().add(jTabbedPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 640, 720));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void generateComponent(){
+        initComponents();
+        setLocationRelativeTo(null);
+        this.setIconImage(AppManager.getImageIcon().getImage());
+        
+        jTabbedPane1.putClientProperty("JTabbedPane.tabAreaAlignment", "fill");
+        jTabbedPane1.putClientProperty("JTabbedPane.tabHeight", 40);
+        
+        jTabbedPane1.setTitleAt(0, "Masuk");
+        jTabbedPane1.setTitleAt(1, "Daftar");
+        
+    }
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel corner;
+    private javax.swing.JLabel image;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
